d81f56eb7111d252c8f834c5215e6ca17c7230d6
priscahlimo <priscahlimoh@gmail.com>
2022-02-13 08:59:54 +0300

    add repo constructor

--- src/app/repo.ts
+++ src/app/repo.ts
@@ -1,2 +1,5 @@
 export class Repo {
+    constructor(public name: string,public html_url:string,public description:string,public language:string, public created_at:Date){
+
+    }
 }
--- src/app/search-http/search-request.service.ts
+++ src/app/search-http/search-request.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Username } from '../username';
 import { Repo } from '../repo';
 import { ProfileComponent } from '../profile/profile.component';
+import { HttpClient } from '@angular/common/http';
 
 
 @Injectable({
@@ -11,7 +12,7 @@ export class SearchRequestService {
   user!:Username;
   repos!:Repo;
   constructor(private http:HttpClient) { 
-    this.user = new User("",0,0,0,"","","");
+    this.user = new Username("",0,0,0,"","","");
     this.repos = new Repo("","","","",new Date());
   }
 }
