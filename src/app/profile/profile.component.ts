07580fda0f004ee98b8291de048474b116c5f29a
priscahlimo <priscahlimoh@gmail.com>
2022-02-13 10:13:13 +0300

    create class properties

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,23 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-// import { Username } from '../username';
-// import { Repo } from '../repo';
-// import { SearchRequestService } from '../search-http/search-request.service';
+import { Username } from '../username';
+import { Repo } from '../repo';
+import { SearchRequestService } from '../search-http/search-request.service';
 
-// @Component({
-//   selector: 'app-profile',
-//   templateUrl: './profile.component.html',
-//   styleUrls: ['./profile.component.css']
-// })
-// export class ProfileComponent implements OnInit {
-//   user!: Username;
-//   repos:any;
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+
+  user!: Username;
+  repos!:Repo;
 
 //   constructor(public searchService: SearchRequestService) {
 
 //    }
 
 //    searchUser(username:string){
-//     this.searchService.getProfile(username).then((success)=>{
+//     this.searchService.getUserProfile(username).then((success)=>{
 //      this.user = this.searchService.user;
 //     },
 //     (error)=>{
@@ -35,4 +36,4 @@ import { Component, OnInit } from '@angular/core';
 //     this.searchUser('priscahlimo');
 //   }
 
-// }
+}
